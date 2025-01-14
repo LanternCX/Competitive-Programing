34d256e818609c8fb904117a9a2ff27921e33768
CaoXin <caoxin@xysu.tech>
2025-01-14 23:45:14 +0800

    feat: NowCoder

--- NowCoder/1008.cpp
+++ NowCoder/1008.cpp
@@ -2,8 +2,6 @@
 using namespace std;
 int main(){
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    for(int i = 0; i < 3; i++){
-        cout << "Welcome to ACM / ICPC!\n";
-    }
+    cout << "\"Genius is 1% inspiration and 99% perspiration.\"";
     return 0;
 }
--- NowCoder/1012.cpp
+++ NowCoder/1012.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main(){
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    for(int i = 0; i < 3; i++){
-        cout << "Welcome to ACM / ICPC!\n";
-    }
+    double n;
+    cin >> n;
+    printf("%.3lf", n);
     return 0;
 }
