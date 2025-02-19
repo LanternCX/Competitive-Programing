7444b30238fb0e3793cfc7fd156d6430f342da49
CaoXin <caoxin@xysu.tech>
2025-02-20 00:30:59 +0800

    fix: gcd, sieve and lca

--- utils/GCD.cpp
+++ utils/GCD.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int a, int b){return a % b == 0 ? b : gcd(b, a % b);}
+int gcd(int a, int b){return b == 0 ? a : gcd(b, a % b);}
 int lcm(int a, int b){return (a * b) / gcd(a, b);}
 
 int main(){
